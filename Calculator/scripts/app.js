58a62ea5e0a954848fa451eb08c4f333c94814b0
Ciantera-Rose <ciantera.rose@gmail.com>
2021-02-17 21:35:48 -0500

    add func to return parseInt on user input, add addition function to show current result and user input with result output, add event listener for + operator

--- Calculator/scripts/app.js
+++ Calculator/scripts/app.js
@@ -1,1 +1,17 @@
-alert('This works!');
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+function getUserNumberInput() {
+  return parseInt(userInput.value);
+}
+
+function add() {
+  const enteredNumber = getUserNumberInput();
+  const calcDescription = `${currentResult} + ${enteredNumber}`;
+  currentResult = currentResult + enteredNumber;
+  outputResult(currentResult, calcDescription);
+}
+
+addBtn.addEventListener('click', add);
+
+//let calculationDescription = `(${defaultResult} + '0 + 10) * 3 / 2 - 1`;
