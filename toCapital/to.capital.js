5382d990ce0156d2684bd01179435e5d352b2dbd
Ciantera-Rose <ciantera.rose@gmail.com>
2021-03-12 16:18:06 -0500

    add new solution for Capitalize first letter in string function

--- toCapital/to.capital.js
+++ toCapital/to.capital.js
@@ -1,13 +1,22 @@
-//Create a Capitalize Function in JavaScript
-const toCapital = str => {
-    const words = str.split(' ');
-    return words.map(word => word[0].toUpperCase() + word.slice(1)).join(" ")
-}
-const shortStr = 'Hi there';
-const longStr = 'the quick brown fox jumped over the lazy dog'
+/*
+Directions:
+Write a function that accepts a string.  The function should
+capitalize the first letter of each word in the string then
+return the capitalized string.
+Examples:
+  capitalize('a short sentence') --> 'A Short Sentence'
+  capitalize('a lazy fox') --> 'A Lazy Fox'
+  capitalize('look, it is working!') --> 'Look, It Is Working!'
+*/
 
+// const toCapital = str => {
+//     const words = str.split(' ');
+//     return words.map(word => word[0].toUpperCase() + word.slice(1)).join(" ")
+// }
+// const shortStr = 'hi there';
+// const longStr = 'the quick brown fox jumped over the lazy dog'
 
-toCapital(longStr); 
+// toCapital(longStr);  //?
 //The Quick Brown Fox Jumped Over The Lazy Dog
 
 /*
@@ -35,3 +44,15 @@ The Quick Brown Fox Jumped Over The Lazy Dog
 The map() method creates a new array populated with the results 
 of calling a provided function on every element in the calling array.
 */
+
+function capitalize(str) {
+  const words = [];
+
+  for (let word of str.split(' ')) {
+    words.push(word[0].toUpperCase() + word.slice(1)); //?
+  }
+  return words.join(' ');
+}
+
+const longSent = 'the quick brown fox jumped over the lazy dog';
+console.log(capitalize(longSent));
